7071c5dfb72bfd9be51b85714cb76a25d1943c3a
arturadamian <arturadamian@gmail.com>
2018-07-30 23:56:04 +0000

    remove before/current

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 # sets up your web servers for the deployment
-mkdir -p /data/web_static/shared/ /data/web_static/releases/test/
-echo -e '<html>\n  <head>\n  </head>\n  <body>\n   Holberton School\n  </body>\n</html>' > /data/web_static/releases/test/index.html
-ln -f -s /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data/
+sudo apt-get update
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/shared/ /data/web_static/releases/test/
+echo "
+<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
+sudo rm -rf /data/web_static/current
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
 sed -i '38i location /hbnb_static { alias /data/web_static/current/; }' /etc/nginx/sites-enabled/default
-service nginx restart
+sudo service nginx restart
