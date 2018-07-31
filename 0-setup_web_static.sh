2d24215630451c9b3601c666ef4eab08d06a1426
arturadamian <arturadamian@gmail.com>
2018-07-31 05:33:06 +0000

    rewrite html

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,16 +3,10 @@
 sudo apt-get update
 sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/shared/ /data/web_static/releases/test/
-echo "
-<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
+echo "Holberton School" > /data/web_static/releases/test/index.html
 sudo rm -rf /data/web_static/current
 sudo ln -s /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sed -i '38i location /hbnb_static { alias /data/web_static/current/; }' /etc/nginx/sites-enabled/default
+allias="\\\tlocation /hbnb_static/ { alias /data/web_static/current/; }"
+sed -i "38i $allias" /etc/nginx/sites-enabled/default
 sudo service nginx restart
